147b4c22f77611f9b86d22476444d7c0a007e869
FukuyamaTokuya <fukuya7711@gmail.com>
2023-03-02 19:40:57 +0900

    validationとテストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   VALID_KANJI_GANA_KANA = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-  VALID_KANA = /\A[ァ-ヶ]+\z/
+  VALID_KANA = /\A[ァ-ヶー]+\z/
   validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :last_name, presence: true, format: { with: VALID_KANJI_GANA_KANA }
   validates :first_name, presence: true, format: { with: VALID_KANJI_GANA_KANA }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -70,8 +70,20 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it 'passwordは半角英数字混合出ないと登録できない' do
+      it 'passwordは半角数字のみでは登録できない' do
         @user.password = '111111'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+      it 'passwordは半角英字のみでは登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+      it 'passwordは全角文字を含むと登録できない' do
+        @user.password = 'アイウエオ1a'
         @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is invalid')
