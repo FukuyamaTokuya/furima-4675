267368c8b1c7607c671d9887c2f64f2567416265
FukuyamaTokuya <fukuya7711@gmail.com>
2023-03-03 22:23:09 +0900

    ActiveHash導入

--- Gemfile
+++ Gemfile
@@ -75,3 +75,4 @@ gem 'image_processing', '~> 1.2'
 
 gem 'net-http'
 
+gem 'active_hash'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,17 @@
 class Item < ApplicationRecord
 
 
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :shipping_id, numericality: { other_than: 1 } 
+  validates :place_id, numericality: { other_than: 1 } 
+  validates :number_of_day_id, numericality: { other_than: 1 } 
+
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :place
+  belongs_to :number_of_day
 end
