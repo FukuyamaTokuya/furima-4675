5a727c5a1f09443c662f6e2b6d2994fb9808b30a
FukuyamaTokuya <fukuya7711@gmail.com>
2023-03-04 10:03:26 +0900

    ログイン状態によっての条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :redirect_root, only: :new
+  
   def index
   end
 
@@ -7,5 +9,22 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item=Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else 
+      render :new
+    end
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name,:description,:category_id,:condition_id,:shipping_fee_id,:place_id,:number_of_day_id,:price,:image).merge(user_id: current_user.id)
+  end
+
+  def redirect_root
+    redirect_to root_path unless user_signed_in?
+  end
+
+
 end
