96772a82b4705a55048486d0e6a66dcc043db11d
FukuyamaTokuya <fukuya7711@gmail.com>
2023-03-07 21:13:01 +0900

    items_controller.rbのリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:show,:edit,:update]
+  
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -19,20 +20,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
     item = Item.find(params[:id]).user_id
-    if user_signed_in? && current_user.id == item
-      @item = Item.find(params[:id])
-    else
+    unless user_signed_in? && current_user.id == item
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(params[:id])
     else
@@ -46,4 +43,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_fee_id, :place_id, :number_of_day_id,
                                  :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
