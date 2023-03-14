ba85981efc9fcad1818d48861b9c04659ef56e94
FukuyamaTokuya <fukuya7711@gmail.com>
2023-03-14 21:51:10 +0900

    rubocop実行

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,9 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
-  
+
   def index
-   if @item.purchase.present? || @item.user_id == current_user.id
-    redirect_to root_path
-   end
+    redirect_to root_path if @item.purchase.present? || @item.user_id == current_user.id
     @purchase_address = PurchaseAddress.new
   end
 
@@ -15,26 +13,29 @@ class PurchasesController < ApplicationController
       pay_item
       @purchase_address.save
       redirect_to root_path
-    else 
+    else
       render :index
     end
   end
 
   private
-    def set_item
-      @item=Item.find(params[:item_id])
-    end
 
-    def purchase_params
-      params.require(:purchase_address).permit(:post_code,:place_id,:city,:house_number,:house_name,:phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token] )
-    end
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
-    def pay_item
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: purchase_params[:token],
-        currency: 'jpy'
-      )
-    end
+  def purchase_params
+    params.require(:purchase_address).permit(:post_code, :place_id, :city, :house_number, :house_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,20 +1,22 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :place_id, :city, :house_number, :house_name, :phone_number ,:token
+  attr_accessor :user_id, :item_id, :post_code, :place_id, :city, :house_number, :house_name, :phone_number, :token
+
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :place_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
-    validates :city, presence: true
-    validates :house_number, presence: true
-    validates :phone_number, presence: true,format: {with: /\A\d{10,11}\z/, message: "is invalid."}
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :place_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid.' }
     validates :token
   end
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
 
-    Address.create(post_code: post_code, place_id: place_id, city: city, house_number: house_number, house_name: house_name, phone_number: phone_number, purchase_id: purchase.id)
+    Address.create(post_code: post_code, place_id: place_id, city: city, house_number: house_number, house_name: house_name,
+                   phone_number: phone_number, purchase_id: purchase.id)
   end
 end
